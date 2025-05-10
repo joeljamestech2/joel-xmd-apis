f143ac8390789018546705cb7463b14bcb0ce8c5
~joeljamestech <162905644+joeljamestech2@users.noreply.github.com>
2025-05-10 13:17:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
         channel_link: 'https://whatsapp.com/channel/0029Vak2PevK0IBh2pKJPp2K',
         channel_jid: '120363317462952356@newsletter',
         thumbnail: 'https://raw.githubusercontent.com/joeljamestech2/JOEL-XMD/refs/heads/main/mydata/media/joelXbot.jpg',
-        image: 'https://i.waifu.pics/-ZeGw8v.gif',
+        image: 'https://files.catbox.moe/8y2ueh.jpg',
         premiumumusers: '[255714595078,255781144539,255767570963,919862726575]',
         caption: '╭─❍「 𝒋𝒐𝒆𝒍 𝒙𝒎𝒅 𝒏𝒐𝒕𝒊𝒇𝒊𝒄𝒂𝒕𝒊𝒐𝒏 」❍\n│\n│ 💡 ᴘʟᴀʏ ᴄᴍᴅs sᴛᴀᴛᴜs: ✅ ᴏɴʟɪɴᴇ\n│\n│ 🧩 ᴀᴠᴀɪʟᴀʙʟᴇ ᴄᴏᴍᴍᴀɴᴅs:\n│ ┌─❍───────────────\n│ │ 🎵 ᴍᴜsɪᴄ   :  .play <song name>\n│ │ 🎥 ᴠɪᴅᴇᴏ   :  .video <video name>\n│ └─❍───────────────\n│\n│ ❓ ʜᴀᴠɪɴɢ ɪssᴜᴇs?\n│ ➤ ᴄᴏɴᴛᴀᴄᴛ ᴛʜᴇ ᴘᴇʀsᴏɴ ᴡʜᴏ ᴅᴇᴘʟᴏʏᴇᴅ ᴛʜɪs ʙᴏᴛ\n│    ꜰᴏʀ ʀᴇᴅᴇᴘʟᴏʏᴍᴇɴᴛ sᴜᴘᴘᴏʀᴛ.\n│\n│ ⚠️ ɴᴏᴛᴇ: ʀᴇᴅᴇᴘʟᴏʏɪɴɢ ᴛʜᴇ ʙᴏᴛ ɪs *ɴᴏᴛ ꜰʀᴇᴇ*.\n│    ʙᴇ ᴘᴏʟɪᴛᴇ, ʙᴇ ᴄᴏᴏʟ!\n│\n╰─❍\n *ᴘᴏᴡᴇʀᴇᴅ ᴡɪᴛʜ ʟᴏᴠᴇ ʙʏ ᴊᴏᴇʟᴊᴀᴍᴇsᴛᴇᴄʜ*'
     };
